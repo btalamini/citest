3d8c6fa1df0f50965c1f5247bfb4aea6985a1f6f
Brandon Talamini <brandon.talamini@gmail.com>
2022-07-03 15:22:36 -0700

    Put actual test pass/fail condition in unit test

--- citest/test/test_functionality.py
+++ citest/test/test_functionality.py
@@ -5,8 +5,9 @@ from citest import functionality
 class TestFunctionality(unittest.TestCase):
 
     def test_square(self):
-        x = 2.0
+        x = 2
         y = functionality.square(x)
+        self.assertEqual(y, 4)
 
 if __name__ == "__main__":
     unittest.main()
